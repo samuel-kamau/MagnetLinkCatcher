66b962eca85d6e6fadc0acbc9c5458ea326f12f7
Pedro Lemos <lemozxpedro@gmail.com>
2019-12-27 01:10:32 -0300

    Deleted design.py due PySimpleGUI limitations

--- Magnet Link Catcher.py
+++ Magnet Link Catcher.py
@@ -5,8 +5,36 @@ except:
 import os
 import pyperclip
 from scripts.get_magnet import GetMagnet
-from gui.design import *
 
+
+sg.LOOK_AND_FEEL_TABLE["MagnetLinkCatcher"] = {
+    'BACKGROUND': "white",
+    'TEXT': "#323232",
+    'INPUT': "#dfe2e8",
+    'TEXT_INPUT': '#000000',
+    'SCROLL': '#c7e78b',
+    'BUTTON': ("white", "#5d2b90"),
+    'PROGRESS': ("white", "black"),
+    'BORDER': 0, 'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0,
+}
+
+sg.change_look_and_feel("MagnetLinkCatcher")
+
+main_layout = [
+    [sg.Text("\n", font=("Segoe UI Light", 5))],
+    [sg.Text("  Magnet Link Catcher", font=("Segoe UI Light", 24),
+             text_color="#5d2b90", justification="left")],
+    [sg.Text("    Search for something", font=("Segoe UI Light", 14))],
+    [sg.Text("\n", font=("Segoe UI Light", 1))],
+    [sg.Text("  "), sg.InputText(size=(28, 6), font=("Segoe UI Light", 12)), sg.VerticalSeparator(pad=(4, (3, 4))),
+        sg.Submit("Search", size=(12, 0), font=("Segoe UI Light", 10, "bold"))],
+    [sg.Text("  "), sg.Button("Support this project", size=(17, 0), font=("Segoe UI Light", 10, "bold")),
+        sg.VerticalSeparator(pad=(6, 3)), sg.Button(
+            "About", size=(7, 0), font=("Segoe UI Light", 10, "bold")),
+        sg.VerticalSeparator(pad=(6, 3)), sg.Button("Exit", size=(12, 0), font=("Segoe UI Light", 10, "bold"))],
+    [sg.Text("\nDeveloped by Pedro Lemos (@pedrolemoz)", font=(
+        "Segoe UI Light", 12), size=(42, 0), justification="center")]
+]
 
 window = sg.Window("Magnet Link Catcher", main_layout, size=(430, 280))
 
@@ -19,11 +47,49 @@ while True:
         window.close()
         break
 
+    if event == "About":
+
+        about_layout = [
+            [sg.Text("\n", font=("Segoe UI Light", 1))],
+            [sg.Text("    This project was born with an idea for automatize torrent downloading.\n    I don't wanna search for torrent and see boring adverts. This application\n    search on Google and return all found magnet links and is able to start\n    the default torrent application, copy links and save its to file.",
+                     font=("Segoe UI", 12), size=(58, 0))],
+            [sg.Text("\n", font=("Segoe UI Light", 1))],
+            [sg.Text(" " * 101), sg.Button("Close", size=(12, 0),
+                                           font=("Segoe UI Light", 10, "bold"))],
+            [sg.Text("\n", font=("Segoe UI Light", 1))]
+        ]
+
+        about_window = sg.Window("About project", about_layout)
+
+        while True:
+            about_event, about_values = about_window.read()
+            if about_event in (None, "Close"):
+                about_window.close()
+                break
+
     if event == "Search":
         download_pages = process.get_download_pages(values[0])
         dict_download_links = process.get_download_links(download_pages)
+        download_links = []
 
         [download_links.append(i) for i in dict_download_links.keys()]
+
+        results_layout = [
+            [sg.Text("\n", font=("Segoe UI Light", 5))],
+            [sg.Text("Process finished sucessfully!", font=(
+                "Segoe UI Light", 14), size=(30, 0), justification="left")],
+            [sg.Text("\n", font=("Segoe UI Light", 1))],
+            [sg.Listbox(values=download_links, size=(90, 15),
+                        font=("Segoe UI", 10), enable_events=True)],
+            [sg.Text("\n", font=("Segoe UI Light", 1))],
+            [sg.Text(" " * 16), sg.Button("Save all links to file", size=(22, 0), font=("Segoe UI Light", 10, "bold")),
+             sg.Button("Open magnet link", size=(16, 0),
+                       font=("Segoe UI Light", 10, "bold")),
+             sg.Button("Copy magnet link", size=(16, 0),
+                       font=("Segoe UI Light", 10, "bold")),
+             sg.Button("Close", size=(12, 0), font=("Segoe UI Light", 10, "bold"))],
+            [sg.Text("\n", font=("Segoe UI Light", 1))]
+        ]
 
         results_window = sg.Window("Sucess!", results_layout)
 
@@ -36,6 +102,16 @@ while True:
 
             if results_event == "Save all links to file":
                 process.magnets_to_file(dict_download_links, values[0])
+
+                save_layout = [
+                    [sg.Text("\n", font=("Segoe UI Light", 5))],
+                    [sg.Text(f"Magnet links saved sucessfully!", size=(
+                        25, 0), font=("Segoe UI Light", 14), justification="left")],
+                    [sg.Text("\n", font=("Segoe UI Light", 1))],
+                    [sg.Text(" " * 6), sg.Button("Open file", size=(12, 0), font=("Segoe UI Light", 10, "bold")),
+                     sg.Button("Close", size=(12, 0), font=("Segoe UI Light", 10, "bold"))],
+                    [sg.Text("\n", font=("Segoe UI Light", 1))]
+                ]
 
                 save_window = sg.Window("Sucess!", save_layout)
 
@@ -55,6 +131,14 @@ while True:
             if results_event == "Copy magnet link":
                 pyperclip.copy(dict_download_links[results_values[0][0]])
 
-                clipboard_window = sg.Window("Sucess!", clipboard_layout, auto_close=True)
+                clipboard_layout = [
+                    [sg.Text("\n", font=("Segoe UI Light", 5))],
+                    [sg.Text("Copied to clipboard!", font=(
+                        "Segoe UI Light", 14), size=(17, 0), justification="left")],
+                    [sg.Text("\n", font=("Segoe UI Light", 1))]
+                ]
+
+                clipboard_window = sg.Window(
+                    "Sucess!", clipboard_layout, auto_close=True)
 
                 clipboard_event, clipboard_result = clipboard_window.read()
