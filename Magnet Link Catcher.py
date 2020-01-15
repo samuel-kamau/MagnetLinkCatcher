556e9549d4e0f49adfeae65faf11c4287d117066
Pedro Lemos <lemozxpedro@gmail.com>
2020-01-14 23:09:14 -0300

    Added setup to build application

--- Magnet Link Catcher.py
+++ Magnet Link Catcher.py
@@ -9,7 +9,7 @@ sg.LOOK_AND_FEEL_TABLE["MagnetLinkCatcher"] = {
     'INPUT': "#dfe2e8",
     'TEXT_INPUT': '#000000',
     'SCROLL': '#c7e78b',
-    'BUTTON': ("white", "#5d2b90"),
+    'BUTTON': ("white", "#DA4453"),
     'PROGRESS': ("white", "black"),
     'BORDER': 0, 'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0,
 }
@@ -18,7 +18,7 @@ sg.change_look_and_feel("MagnetLinkCatcher")
 
 main_layout = [
     [sg.Text("\n", font=("Segoe UI Light", 5))],
-    [sg.Text("  Magnet Link Catcher", font=("Segoe UI Light", 24), text_color="#5d2b90", justification="left")],
+    [sg.Text("  Magnet Link Catcher", font=("Segoe UI Light", 24), text_color="#DA4453", justification="left")],
     [sg.Text("    Search for something", font=("Segoe UI Light", 14))],
     [sg.Text("\n", font=("Segoe UI Light", 1))],
     [sg.Text("  "), sg.InputText(size=(28, 6), font=("Segoe UI Light", 12)), sg.VerticalSeparator(pad=(4, (3, 4))), sg.Submit("Search", size=(12, 0), font=("Segoe UI Light", 10, "bold"))],
@@ -32,7 +32,7 @@ main_layout = [
     [sg.Text("\nDeveloped by Pedro Lemos (@pedrolemoz)", font=("Segoe UI Light", 12), size=(42, 0), justification="center")]
 ]
 
-window = sg.Window("Magnet Link Catcher", main_layout, size=(430, 410))
+window = sg.Window("Magnet Link Catcher", main_layout, size=(430, 410), icon="icon.ico")
 
 process = GetMagnet()
 
@@ -53,7 +53,7 @@ while True:
             [sg.Text("\n", font = ("Segoe UI Light", 1))]
         ]
 
-        about_window = sg.Window("About project", about_layout)
+        about_window = sg.Window("About project", about_layout, icon="icon.ico")
 
         while True:
             about_event, about_values = about_window.read()
@@ -78,7 +78,7 @@ while True:
             [sg.Text("\n", font=("Segoe UI Light", 1))]
         ]
 
-        results_window = sg.Window("Sucess!", results_layout)
+        results_window = sg.Window("Sucess!", results_layout, icon="icon.ico")
 
         while True:
             results_event, results_values = results_window.read()
@@ -100,7 +100,7 @@ while True:
                     [sg.Text("\n", font=("Segoe UI Light", 1))]
                 ]
 
-                save_window = sg.Window("Sucess!", save_layout)
+                save_window = sg.Window("Sucess!", save_layout, icon="icon.ico")
 
                 while True:
                     save_event, save_result = save_window.read()
@@ -124,6 +124,6 @@ while True:
                     [sg.Text("\n", font=("Segoe UI Light", 1))]
                 ]
 
-                clipboard_window = sg.Window("Sucess!", clipboard_layout, auto_close=True)
+                clipboard_window = sg.Window("Sucess!", clipboard_layout, auto_close=True, icon="icon.ico")
 
                 clipboard_event, clipboard_result = clipboard_window.read()
---
+++ build_application.py
@@ -0,0 +1,21 @@
+from cx_Freeze import setup, Executable
+
+target = Executable(
+    script= "Magnet Link Catcher.py",
+    base = "Win32GUI",
+    icon = "icon.ico"
+    )
+
+setup(
+    name = "Magnet Link Catcher",
+    version = "1.0",
+    description = "Get magnet links from internet without any effort!",
+    author = "Pedro Lemos",
+    options = {"build_exe": {
+    				"optimize": 1,
+    				"packages": ["os", "tkinter", "PySimpleGUI", "pyperclip", "requests", "urllib.parse", "bs4"],
+    				"include_files": ["icon.ico"]
+    		}
+    	},
+    executables = [target]
+    )
