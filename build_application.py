8ee0f3385ade6eaf0f5f6ad766290a453e0b593b
Pedro Lemos <lemozxpedro@gmail.com>
2020-01-15 01:23:46 -0300

    Minor changes

--- build_application.py
+++ build_application.py
@@ -1,21 +1,23 @@
 from cx_Freeze import setup, Executable
 
-target = Executable(
-    script= "Magnet Link Catcher.py",
+target = Executable (
+    script = "Magnet Link Catcher.py",
     base = "Win32GUI",
     icon = "icon.ico"
     )
 
-setup(
+setup (
     name = "Magnet Link Catcher",
     version = "1.0",
     description = "Get magnet links from internet without any effort!",
     author = "Pedro Lemos",
-    options = {"build_exe": {
-    				"optimize": 1,
-    				"packages": ["os", "tkinter", "PySimpleGUI", "pyperclip", "requests", "urllib.parse", "bs4"],
-    				"include_files": ["icon.ico"]
-    		}
-    	},
+    options = {
+                "build_exe": {
+                        "optimize": 1,
+                        "packages": ["os", "tkinter", "PySimpleGUI", "pyperclip", "requests", "urllib.parse", "bs4"],
+                        "include_files": ["icon.ico"]
+                    }
+            },
+    
     executables = [target]
     )
